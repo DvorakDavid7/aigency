f2ae9ab0453ee156988c3acfbfddf4dd640567bb
David Dvořák <dvorakdavid7@gmail.com>
2026-02-11 08:40:27 +0100

    add shadcn mcp

--- resources/js/pages/Test.vue
+++ resources/js/pages/Test.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+
+</script>
+
 <template>
     <div>
+        <Button>click me</Button>
         <a href="/auth/redirect">
             connect facebook
         </a>
